46b12f98aff78bbca172215c528210aa6260d4c6
Sophie Engle <sjengle@cs.usfca.edu>
2017-02-13 11:12:49 -0800

    Fixing src vs template

--- OOP Principles/templates/HashCodeDemo.java
+++ OOP Principles/templates/HashCodeDemo.java
@@ -1,202 +1,82 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-/**
- * Demonstrates how parameter passing works in Java, including the difference in
- * behavior for immutable versus mutable objects.
- *
- * In particular, mutability is tied to how memory is handled. If new memory is
- * created when you change a value, then the original object is immutable. If
- * the values stored at the original memory location is modified, then it is
- * mutable.
- *
- * This is also related to whether it is "safe" to pass objects, because if it
- * is mutable, other classes can modify its value using that reference!
- */
 public class HashCodeDemo {
 
-	/**
-	 * Shows the actual hash codes and the String representation of the
-	 * specified object.
-	 *
-	 * Note the usage of System.identityHashCode(object) instead of
-	 * Object.hashCode(). This returns the value from the default hashcode
-	 * implementation, which gives the memory address of where the object is
-	 * currently stored. Most classes override this implementation.
-	 *
-	 * It is not the focus of this example!
-	 *
-	 * @param label
-	 *            - label to use in output
-	 * @param object
-	 *            - object to display
-	 */
 	public static void printHash(String label, Object object) {
 		System.out.printf("%-9s : x%08X : %s %n", label, System.identityHashCode(object), object);
 	}
 
-	/**
-	 * A special version of {@link #printHash(String, Object)} that works for
-	 * Object arrays.
-	 *
-	 * @see #printHash(String, Object)
-	 * @param label
-	 *            - label to use in output
-	 * @param object
-	 *            - object array to display
-	 */
 	public static void printHash(String label, Object[] object) {
 		System.out.printf("%-9s : x%08X : %s %n", label, System.identityHashCode(object), Arrays.toString(object));
 	}
 
-	/**
-	 * Used to demonstrate how the memory location is or is not modified within
-	 * a method call for this type of object.
-	 *
-	 * @param value
-	 *            - value passed in from main method
-	 */
 	public static void testInteger(Integer value) {
 		printHash("INNER BEG", value);
-		value = 1;
+		// TODO
 		printHash("INNER END", value);
 	}
 
-	/**
-	 * Used to demonstrate how the memory location is or is not modified within
-	 * a method call for this type of object.
-	 *
-	 * @param value
-	 *            - value passed in from main method
-	 */
 	public static void testString(String value) {
 		printHash("INNER BEG", value);
-		value = value.toUpperCase(); // notice the assignment!
+		// TODO
 		printHash("INNER END", value);
 	}
 
-	/**
-	 * Used to demonstrate how the memory location is or is not modified within
-	 * a method call for this type of object.
-	 *
-	 * @param value
-	 *            - value passed in from main method
-	 */
 	public static void testBuilder(StringBuilder value) {
 		printHash("INNER BEG", value);
-		value.append(" banana");
+		// TODO
 		printHash("INNER END", value);
 	}
 
-	/**
-	 * Used to demonstrate how the memory location is or is not modified within
-	 * a method call for this type of object.
-	 *
-	 * @param value
-	 *            - value passed in from main method
-	 */
 	public static void testArray(String[] value) {
 		printHash("INNER BEG", value);
-		value[0] = "carrot";
+		// TODO
 		printHash("INNER END", value);
 	}
 
-	/**
-	 * Used to demonstrate how the memory location is or is not modified within
-	 * a method call for this type of object.
-	 *
-	 * @param value
-	 *            - value passed in from main method
-	 */
 	public static void testList(ArrayList<String> value) {
 		printHash("INNER BEG", value);
-		value.add("banana");
+		// TODO
 		printHash("INNER BEG", value);
 	}
 
 	public static void main(String[] args) {
 
-		/*
-		 * Objects in Java may be mutable -OR- immutable. Often, you can find
-		 * this information in the API.
-		 *
-		 * When changing the value of an immutable object, you are actually
-		 * creating a new object. The reference to the old object is broken and
-		 * updated to the new object. The garbage collector will clean up the
-		 * now unused space of the old object.
-		 *
-		 * Notice how the memory address changes once the Integer value is
-		 * modified and reassigned! The original Integer object is not being
-		 * modified.
-		 */
-
-		Integer a = new Integer(0);
+		Integer a = null; // TODO
 		printHash("OUTER BEG", a);
 		testInteger(a);
 		printHash("OUTER END", a);
 		System.out.println();
 
-		/*
-		 * Immutable objects with the same value might as well share the same
-		 * memory location. This allows Java to optimize its memory usage. That
-		 * is also why you should avoid calling "new" when it comes to immutable
-		 * objects.
-		 */
+//		Integer b = null; // TODO
+//		printHash("VALUE OF", b);
+//		printHash("CALL NEW", new Integer(1));
+//		System.out.println();
 
-		Integer b = Integer.valueOf(1);
-		printHash("VALUE OF", b);
-		printHash("CALL NEW", new Integer(1));
-		System.out.println();
+//		String c = null; // TODO
+//		printHash("OUTER BEG", c);
+//		testString(c);
+//		printHash("OUTER END", c);
+//		System.out.println();
 
-		/*
-		 * Strings are immutable objects! As such, their references are safe to
-		 * pass since any modification to that String will break the reference
-		 * to the original object.
-		 */
+//		StringBuilder d = null; // TODO
+//		printHash("OUTER BEG", d);
+//		testBuilder(d);
+//		printHash("OUTER END", d);
+//		System.out.println();
 
-		String c = "apple";
-		printHash("OUTER BEG", c);
-		testString(c);
-		printHash("OUTER END", c);
-		System.out.println();
+//		String[] e = null; // TODO
+//		printHash("OUTER BEG", e);
+//		testArray(e);
+//		printHash("OUTER END", e);
+//		System.out.println();
 
-		/*
-		 * Because of this, if you need to make a lot of modification to a
-		 * String, use a StringBuilder instead (or a StringBuffer if you are
-		 * using multiple threads).
-		 *
-		 * StringBuilder is mutable, so the function may modify the value of the
-		 * original object directly through the passed reference.
-		 */
-
-		StringBuilder d = new StringBuilder("apple");
-		printHash("OUTER BEG", d);
-		testBuilder(d);
-		printHash("OUTER END", d);
-		System.out.println();
-
-		/*
-		 * Arrays are mutable objects in Java. (You see the "new" keyword after
-		 * all, don't you?)
-		 */
-
-		String[] e = new String[] { "apple", "banana" };
-		printHash("OUTER BEG", e);
-		testArray(e);
-		printHash("OUTER END", e);
-		System.out.println();
-
-		/*
-		 * The same holds for an ArrayList and other Collection objects. These
-		 * objects are mutable, so passing a reference can be dangerous. A
-		 * function could clear the collection completely through the reference.
-		 */
-
-		ArrayList<String> f = new ArrayList<String>();
-		f.add("apple");
-		printHash("OUTER BEG", f);
-		testList(f);
-		printHash("OUTER END", f);
+//		ArrayList<String> f = null; // TODO
+//		f.add("apple");
+//		printHash("OUTER BEG", f);
+//		testList(f);
+//		printHash("OUTER END", f);
 	}
 
 }
--- OOP Principles/templates/ObjectDemo.java
+++ OOP Principles/templates/ObjectDemo.java
@@ -1,98 +1,22 @@
-import java.util.Arrays;
-
-/**
- * This class demonstrates the {@link Object} class, that arrays are objects and
- * hence inherit those methods.
- */
 public class ObjectDemo {
 
-	/**
-	 * This method demonstrates that we can create an actual {@link Object} if
-	 * we wanted to, and access various properties about that object.
-	 *
-	 * @see {@link Object}
-	 */
-	public static void objectDemo() {
-		Object demo = new Object();
+	public static void demo() {
+		Object demo = null; // TODO
 
-		System.out.printf("%s%n", demo.toString());
-		System.out.printf("%s%n", demo.getClass().getName());
-		System.out.printf("%x%n", demo.hashCode());
-		System.out.printf("%n");
+		// TODO
 	}
 
-	/**
-	 * This method demonstrates that arrays are also considered objects, and
-	 * have access to the methods in {@link Object}. The key here is to note
-	 * that arrays require the {@code new} keyword to create.
-	 *
-	 * The default {@code toString()} implementation is not very informative.
-	 * Instead, we have to use {@link Arrays#toString(int[])} to get useful
-	 * output for arrays.
-	 *
-	 * @see {@link Object}
-	 * @see {@link Arrays}
-	 */
-	public static void arrayDemo() {
-		int[] demo = new int[] { 1, 2, 3 };
+//		System.out.printf("demo1 = %x%n", demo1.hashCode());
+//		System.out.printf("demo2 = %x%n", demo2.hashCode());
+//		System.out.printf("demo3 = %x%n", demo3.hashCode());
+//		System.out.printf("%n");
+//
+//		System.out.printf("demo1 == demo2 = %s%n", demo1 == demo2);
+//		System.out.printf("demo2 == demo3 = %s%n", demo2 == demo3);
+//		System.out.printf("demo2.equals(demo3) = %s%n", demo2.equals(demo3));
+//		System.out.printf("%n");
 
-		System.out.printf("%s%n", demo.toString());
-		System.out.printf("%s%n", demo.getClass().getName());
-		System.out.printf("%x%n", demo.hashCode());
-		System.out.printf("%s%n", Arrays.toString(demo));
-		System.out.printf("%n");
-	}
-
-	/**
-	 * This method demonstrates that any class, including this one, is an object
-	 * and hence has access to the methods in {@link Object}. We have not
-	 * created a constructor, but we can still create an object of this class!
-	 * The default constructor in the {@link Object} class will be called in
-	 * this case.
-	 *
-	 * @see {@link Object}
-	 */
-	public static void thisDemo() {
-		ObjectDemo demo = new ObjectDemo();
-
-		System.out.printf("%s%n", demo.toString());
-		System.out.printf("%s%n", demo.getClass().getName());
-		System.out.printf("%x%n", demo.hashCode());
-		System.out.printf("%n");
-	}
-
-	/**
-	 * The default {@code equals())} method in the {@link Object} class compares
-	 * hash codes, so if the hash codes are equal then the objects are equal. By
-	 * default, the {@code ==} operator does the same.
-	 */
-	public static void equalsDemo() {
-		Object demo1 = new Object();
-		Object demo2 = new Object();
-		Object demo3 = demo2;
-
-		System.out.printf("demo1 = %x%n", demo1.hashCode());
-		System.out.printf("demo2 = %x%n", demo2.hashCode());
-		System.out.printf("demo3 = %x%n", demo3.hashCode());
-		System.out.printf("%n");
-
-		System.out.printf("demo1 == demo2 = %s%n", demo1 == demo2);
-		System.out.printf("demo2 == demo3 = %s%n", demo2 == demo3);
-		System.out.printf("demo2.equals(demo3) = %s%n", demo2.equals(demo3));
-		System.out.printf("%n");
-	}
-
-	/**
-	 * Runs all of the demos. Comment out the demos if you want to run them one
-	 * at a time.
-	 *
-	 * @param args
-	 *            unused
-	 */
 	public static void main(String[] args) {
-		objectDemo();
-		arrayDemo();
-		thisDemo();
-		equalsDemo();
+		demo();
 	}
 }
